3995647225575d75e4f6892a149340baf06047ec
xihi <164637584@qq.com>
2018-02-21 19:12:55 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,14 @@
 
 #创建文件夹
 mkdir -p /etc/storage/dnsmasq/dns/conf
+
+#下载dnsmasq规则
+cd /etc/storage/dnsmasq/dns/conf
+wget --no-check-certificate https://raw.githubusercontent.com/googlehosts/hosts/master/hosts-files/dnsmasq.conf -O dnsmasq.conf;sed -i "1 i\## Downloaded：$(date "+%Y-%m-%d %H:%M:%S")" dnsmasq.conf
+wget --no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/ip.conf -O ip.conf;sed -i "1 i\## Downloaded：$(date "+%Y-%m-%d %H:%M:%S")" ip.conf
+wget --no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/union.conf -O union.conf;sed -i "1 i\## Downloaded：$(date "+%Y-%m-%d %H:%M:%S")" union.conf
+wget --no-check-certificate https://raw.githubusercontent.com/xuhui0607/dnsmasq/master/dnslb -O /etc/storage/dnsmasq/dns/dnslb;sed -i "1 i\## Downloaded：$(date "+%Y-%m-%d %H:%M:%S")" /etc/storage/dnsmasq/dns/dnslb
+
 ##下载hosts规则
 cd /etc/storage/dnsmasq/dns
 wget --no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/hosts -O hosts
@@ -21,12 +29,6 @@ wait
 sed -i "1 i\## Download：\$(date "+%Y-%m-%d %H:%M:%S")" hosts
 
 
-#下载dnsmasq规则
-cd /etc/storage/dnsmasq/dns/conf
-wget --no-check-certificate https://raw.githubusercontent.com/googlehosts/hosts/master/hosts-files/dnsmasq.conf -O dnsmasq.conf;sed -i "1 i\## Downloaded：$(date "+%Y-%m-%d %H:%M:%S")" dnsmasq.conf
-wget --no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/ip.conf -O ip.conf;sed -i "1 i\## Downloaded：$(date "+%Y-%m-%d %H:%M:%S")" ip.conf
-wget --no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/union.conf -O union.conf;sed -i "1 i\## Downloaded：$(date "+%Y-%m-%d %H:%M:%S")" union.conf
-wget --no-check-certificate https://raw.githubusercontent.com/xuhui0607/dnsmasq/master/dnslb -O /etc/storage/dnsmasq/dns/dnslb;sed -i "1 i\## Downloaded：$(date "+%Y-%m-%d %H:%M:%S")" /etc/storage/dnsmasq/dns/dnslb
 #重启dnsmasq
 restart_dhcpd
 
